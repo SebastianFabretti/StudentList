4ade325af1afef40b216d84342ecc513123488fa
Sebb <sebastianfabretti@gmail.com>
2023-11-29 23:20:51 -0300

    Routing agregado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EstudiantesListadoComponent } from './components/estudiantes-listado/estudiantes-listado.component';
+import { RoutingComponent } from './components/routing/routing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'EstudiantesListadoComponent', component: EstudiantesListadoComponent},
+  {path: 'RoutingTestComponent', component: RoutingComponent}
+
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { EstudiantesListadoComponent } from './components/estudiantes-listado/estudiantes-listado.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RoutingComponent } from './components/routing/routing.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EstudiantesListadoComponent,
+    RoutingComponent,
   ],
   imports: [
     BrowserModule,
